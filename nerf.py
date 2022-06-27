18b527728c3a324f6b3aa6e5c6dc314b895c879e
Tabitha Oanda <50864401+TabithaKO@users.noreply.github.com>
2022-06-27 10:57:52 -0700

    Added Edited files

--- nerf.py
+++ nerf.py
@@ -667,7 +667,6 @@ def train():
             print('test poses shape', render_poses.shape)
 
             rgbs, _ = render_path(render_poses, hwf, K, args.chunk, render_kwargs_test, gt_imgs=images, savedir=testsavedir, render_factor=args.render_factor)
-            # here's where to calculate the new PSNR
             print('Done rendering', testsavedir)
             imageio.mimwrite(os.path.join(testsavedir, 'video.mp4'), to8b(rgbs), fps=30, quality=8)
 
